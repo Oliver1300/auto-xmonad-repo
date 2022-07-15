e8def7d26539a286d2d581983f6ef65d7584d4ea
Oliver1300 <olivercabrera40.oc@gmail.com>
2022-07-15 22:39:12 +0100

    Update automationsteps.sh

--- automationsteps.sh
+++ automationsteps.sh
@@ -1,16 +1,18 @@
 # NOTES: Installs git
 
 # xmonad installation, following steps from https://xmonad.org/INSTALL.html
-### installs xmonad dependencies
+
+# installs xmonad dependencies
 sudo pacman -S git xorg-server xorg-apps xorg-xinit xorg-xmessage libx11 libxft libxinerama libxrandr libxss pkgconf git
 sleep 1
-### xmonad preparation (dotfiles)
+# xmonad preparation (dotfiles)
 mkdir -p ~/.config/xmonad && cd ~/.config/xmonad
 git clone https://github.com/xmonad/xmonad
 git clone https://github.com/xmonad/xmonad-contrib
 sleep 1
+
 # build xmonad using Stack
-## installing stack
+# installing stack
 sudo pacman -S stack
 sleep 1
 stack upgrade
