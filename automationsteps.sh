38d4257788cb32da2fed4d2e9e67419b023c766e
Oliver1300 <olivercabrera40.oc@gmail.com>
2022-07-16 00:32:42 +0100

    Update automationsteps.sh

--- automationsteps.sh
+++ automationsteps.sh
@@ -1,5 +1,5 @@
-https://xmonad.org/INSTALL.html
-https://www.youtube.com/watch?v=XvKAiR403O0
+#https://xmonad.org/INSTALL.html
+#https://www.youtube.com/watch?v=XvKAiR403O0
 # Make sure you are in ~/  use pwd to see it
 # Get this script via: git clone https://github.com/Oliver1300/auto-xmonad-repo
 # NOTES: Installs git
@@ -8,24 +8,32 @@ https://www.youtube.com/watch?v=XvKAiR403O0
 
 # install xmonad dependencies
 sudo pacman -S git xorg-server xorg-apps xorg-xinit xorg-xmessage libx11 libxft libxinerama libxrandr libxss pkgconf git
-sleep 1
+
 # xmonad preparation (dotfiles)
 mkdir -p ~/.config/xmonad
 mv ~/auto-xmonad-repo/xmonad.hs ~/.config/xmonad
 cd ~/.config/xmonad
+clear
 ls
-echo 'You should see now xmonad.hs'
+# AUTOMATE THIS WITH GREP
+echo 'You should see now xmonad.hs printed'
 sleep 5
 
 git clone https://github.com/xmonad/xmonad
 git clone https://github.com/xmonad/xmonad-contrib
-sleep 1
+# add ~/.local/bin to $PATH in zsh
+if [ echo $0 == "zsh" ] ; then
+
+else
+  echo 'Not found zsh shell, could lead to an error in the future'
+  echo 'Continuing in 10 seconds'
+  sleep 10
+fi
+source ~/.zshrc
 
 # build xmonad using Stack
 # installing stack
 sudo pacman -S stack
 sleep 1
 stack upgrade
-sleep 1
 stack init
-sleep 2
