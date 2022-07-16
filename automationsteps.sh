f733df4d54d92a2194081a247e062977e57008b8
Oliver1300 <olivercabrera40.oc@gmail.com>
2022-07-16 02:25:06 +0100

    Update automationsteps.sh

--- automationsteps.sh
+++ automationsteps.sh
@@ -1,18 +1,13 @@
 #https://xmonad.org/INSTALL.html
 #https://www.youtube.com/watch?v=XvKAiR403O0
-
 # Make sure you are in ~/  use pwd to see it
 # Get this script via: git clone https://github.com/Oliver1300/auto-xmonad-repo
-
 # xmonad installation, following steps from https://xmonad.org/INSTALL.html
-
 # install xmonad dependencies
 sudo pacman -Syu
 sudo pacman -S --needed git xorg-server xorg-apps xorg-xinit xorg-xmessage libx11 libxft libxinerama libxrandr libxss pkgconf git zsh
-
 #configure zsh
 zsh
-
 # xmonad preparation (dotfiles)
 mkdir -p ~/.config/xmonad
 mv ~/auto-xmonad-repo/xmonad.hs ~/.config/xmonad
@@ -22,7 +17,6 @@ ls
 # AUTOMATE THIS WITH GREP
 echo 'You should see now xmonad.hs printed'
 sleep 5
-
 git clone https://github.com/xmonad/xmonad
 git clone https://github.com/xmonad/xmonad-contrib
 # add ~/.local/bin to $PATH in zsh
@@ -30,9 +24,7 @@ echo '# set PATH so it includes user local bin if it exists' >> ~./zshrc
 echo 'if [ -d "$HOME/.local/bin" ]; then' >> ~./zshrc
 echo '  PATH="$HOME/.local/bin:$PATH"' >> ~./zshrc
 echo 'fi' >> ~./zshrc
-
 source ~/.zshrc
-
 # build xmonad using Stack
 # installing stack
 sudo pacman -S stack
