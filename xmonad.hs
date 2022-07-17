58ba6fd84c6a014adc73b0e1a5ad8e8afcf16ea6
Oliver1300 <olivercabrera40.oc@gmail.com>
2022-07-17 16:00:24 +0100

    Update xmonad.hs

--- xmonad.hs
+++ xmonad.hs
@@ -2,6 +2,6 @@ import XMonad
 
 main :: IO ()
 main = xmonad def
-  { terminal = "alacritty" -- changes the default terminal to alacritty NO TESTED YET
-  , modMask = mod4Mask -- changes the mod key from default ALT to SUPER(windows key)
+  { terminal = "alacritty" -- changes the default terminal to alacritty
+  , modMask = mod4Mask -- changes the mod key from ALT to SUPER key
   }
